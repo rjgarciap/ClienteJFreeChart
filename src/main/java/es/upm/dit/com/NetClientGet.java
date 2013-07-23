d8d47f10bdb3de4b1118b056baba594fbf23ffde
ricardo <ricardo@ricardo-VirtualBox.(none)>
2013-07-23 16:55:01 +0200

    First commit

--- src/main/java/es/upm/dit/com/NetClientGet.java
+++ src/main/java/es/upm/dit/com/NetClientGet.java
@@ -1,4 +1,5 @@
 package es.upm.dit.com;
+
 import java.io.BufferedReader;
 
 import org.jfree.chart.ChartFactory;
@@ -20,104 +21,128 @@ import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 
- 
 public class NetClientGet {
- 
+
+	// Metodo que genera y hace visible el frame de la grafica - Tipo Barras
+	public static void generaGrafica(String titulo, String x, String y,
+			DefaultCategoryDataset dataset) {
+		JFreeChart chart = ChartFactory.createBarChart(titulo, x, y, dataset, // Dataset
+				PlotOrientation.VERTICAL, true, true, false);
+
+		// crear y visualizar una ventana...
+		ChartFrame frame = new ChartFrame("Grafica " + titulo, chart);
+
+		frame.pack();
+		frame.setVisible(true);
+
+	}
+
 	// http://localhost:8080/RESTfulExample/json/product/get
 	public static void main(String[] args) throws Exception {
- 
 
- 
 		URL url = new URL("http://glacial-refuge-5887.herokuapp.com/json");
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 		conn.setRequestProperty("Accept", "application/json");
- 
+
 		if (conn.getResponseCode() != 200) {
 			throw new RuntimeException("Failed : HTTP error code : "
 					+ conn.getResponseCode());
 		}
- 
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(
-			(conn.getInputStream())));
-		String textoJSON="";
+				(conn.getInputStream())));
+		String textoJSON = "";
 		String output;
 		System.out.println("Output from Server .... \n");
 		while ((output = br.readLine()) != null) {
-			textoJSON+=output;
+			textoJSON += output;
 			System.out.println(output);
 		}
- 
-		conn.disconnect();
- 
-	  DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-	  DefaultCategoryDataset dataset2 = new DefaultCategoryDataset();
-	  JSONObject json = (JSONObject) JSONSerializer.toJSON(textoJSON);
-	  JSONArray medidas = json.getJSONArray("medidas");
-	  //Scanner para pedir las
-	  Scanner nuevoScanner=new Scanner(System.in);
-	  System.out.println("¿Que gŕafica quieres?");
-	  System.out.println("1.-Freememory");
-	  System.out.println("2.-CPUtimes");
-	  int tipoGrafica=nuevoScanner.nextInt();
-	  /**/
-	   
-	  JFreeChart chart=null;
-	  ChartFrame frame=null;
-      switch (tipoGrafica){
-      	case 1:
-      		for (int i=0;i<medidas.size();i++){
-      		  dataset.addValue(medidas.getJSONObject(i).getLong("freememory"),"Medida",medidas.getJSONObject(i).getString("date"));
-      	  }
-      		chart = ChartFactory.createBarChart("Free Memory",
-    				"Fecha",
-    				"Bytes",
-    				dataset,	//Dataset
-    				PlotOrientation.VERTICAL,
-    				true,
-    				true,
-    				false);
-    		//crear y visualizar una ventana...
-    		frame = new ChartFrame("Grafica Free Memory", chart);
-      		break;
-      	case 2:
-      		JSONObject cputimes=null;
-      	  
-      	  for(int i=0;i<medidas.size();i++){
-      		  cputimes =medidas.getJSONObject(i).getJSONObject("cputimes");
-      		  
-      		  for(int j=0;j<4;j+=1){
-      			Iterator iterator=cputimes.keys();
-      			  while(iterator.hasNext()){
-      				  Object clave =iterator.next();
-      				  dataset2.addValue(((JSONArray)cputimes.get(clave)).getLong(j), clave.toString(), "Core:"+(j+1)+"-"+medidas.getJSONObject(i).getString("date"));
-      			  }
-      		  }
-      	  }
-      	chart = ChartFactory.createBarChart("CPU Times",
-				"Fecha",
-				"Tiempo",
-				dataset2,	//Dataset
-				PlotOrientation.VERTICAL,
-				true,
-				true,
-				false);
-		//crear y visualizar una ventana...
-		frame = new ChartFrame("Grafica CPU Times", chart);
-      		break;
-      }	 
-	  
-	//Crear el dataset...
-		
-		
-		//Crear el gráfico...
-		
-		frame.pack();
-		frame.setVisible(true);
-	  
-	}	
-	  
-	
 
-	  
+		conn.disconnect();
+
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		DefaultCategoryDataset dataset2 = new DefaultCategoryDataset();
+		JSONObject json = (JSONObject) JSONSerializer.toJSON(textoJSON);
+		JSONArray medidas = json.getJSONArray("medidas");
+		// Scanner para recibir ordenes
+		Scanner nuevoScanner = new Scanner(System.in);
+		System.out.println("¿Que gŕafica quieres?(Introduce el numero)");
+		System.out.println("1 - Freememory");
+		System.out.println("2 - CPUtimes");
+		System.out.println("3 - Todas");
+
+		int tipoGrafica = nuevoScanner.nextInt();
+
+		switch (tipoGrafica) {
+		case 1:
+			for (int i = 0; i < medidas.size(); i++) {
+				dataset.addValue(
+						medidas.getJSONObject(i).getLong("freememory"),
+						"Medida", medidas.getJSONObject(i).getString("date"));
+			}
+			generaGrafica("Free Memory", "Fecha", "Bytes", dataset);
+
+			break;
+		case 2:
+			JSONObject cputimes = null;
+
+			for (int i = 0; i < medidas.size(); i++) {
+				cputimes = medidas.getJSONObject(i).getJSONObject("cputimes");
+
+				for (int j = 0; j < 4; j += 1) {
+					Iterator iterator = cputimes.keys();
+					while (iterator.hasNext()) {
+						Object clave = iterator.next();
+						dataset2.addValue(
+								((JSONArray) cputimes.get(clave)).getLong(j),
+								clave.toString(),
+								"Core:"
+										+ (j + 1)
+										+ "-"
+										+ medidas.getJSONObject(i).getString(
+												"date"));
+					}
+				}
+			}
+			generaGrafica("CPU Times", "Fecha", "Tiempo", dataset2);
+
+			break;
+		case 3:
+			// Primera grafica
+			for (int i = 0; i < medidas.size(); i++) {
+				dataset.addValue(
+						medidas.getJSONObject(i).getLong("freememory"),
+						"Medida", medidas.getJSONObject(i).getString("date"));
+			}
+			generaGrafica("Free Memory", "Fecha", "Bytes", dataset);
+
+			// Segunda grafica
+			JSONObject cputimes2 = null;
+
+			for (int i = 0; i < medidas.size(); i++) {
+				cputimes2 = medidas.getJSONObject(i).getJSONObject("cputimes");
+
+				for (int j = 0; j < 4; j += 1) {
+					Iterator iterator = cputimes2.keys();
+					while (iterator.hasNext()) {
+						Object clave = iterator.next();
+						dataset2.addValue(
+								((JSONArray) cputimes2.get(clave)).getLong(j),
+								clave.toString(),
+								"Core:"
+										+ (j + 1)
+										+ "-"
+										+ medidas.getJSONObject(i).getString(
+												"date"));
+					}
+				}
+			}
+			generaGrafica("CPU Times", "Fecha", "Tiempo", dataset2);
+			break;
+		}
+
+	}
+
 }
